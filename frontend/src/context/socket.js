4459672b28af7fc69a41f62c211a4279b17968ae
Byts Maxim <maxim.90svet@gmail.com>
2024-05-01 20:16:04 +0300

    refactor index.jsx

--- frontend/src/context/socket.js
+++
@@ -1,5 +0,0 @@
-/* eslint import/prefer-default-export: 0 */
-
-import React from 'react';
-
-export const SocketContext = React.createContext();
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,33 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.css';
-import i18next from 'i18next';
-import { io } from 'socket.io-client';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
 import { initReactI18next, I18nextProvider } from 'react-i18next';
-import * as LeoProfanity from 'leo-profanity';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import i18next from 'i18next';
+import * as LeoProfanity from 'leo-profanity';
+import { io } from 'socket.io-client';
 
+import App from './components/App.jsx';
+import { actions as channelsActions } from './slices/channels.js';
+import { actions as messagesActions } from './slices/messages.js';
 import resources from './locales/index.js';
 import store from './slices/index.js';
-import App from './components/App.jsx';
-import { SocketContext } from './context/socket';
-
-import reportWebVitals from './reportWebVitals.js';
-import { actions as messagesActions } from './slices/messages.js';
-import { actions as channelsActions } from './slices/channels.js';
 import { LeoProfanityContext } from './context/filter.js';
 
-i18next
-  .use(initReactI18next)
-  .init({
-    resources,
-    lng: 'ru',
-    fallbackLng: 'ru',
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+import reportWebVitals from './reportWebVitals.js';
 
 const rollbarConfig = {
   accessToken: process.env.ACCESS_TOKEN,
@@ -36,47 +24,64 @@ const rollbarConfig = {
   environment: 'production',
 };
 
-const socket = io();
+const i18nextConfig = {
+  resources,
+  lng: 'ru',
+  fallbackLng: 'ru',
+  interpolation: {
+    escapeValue: false,
+  },
+};
 
-const filter = LeoProfanity;
+const subscribeToServerEvents = () => {
+  const socket = io();
 
-socket.on('newMessage', (payload) => {
-  store.dispatch(messagesActions.addMessage(payload));
-});
+  socket.on('newMessage', (payload) => {
+    store.dispatch(messagesActions.addMessage(payload));
+  });
 
-socket.on('newChannel', (payload) => {
-  store.dispatch(channelsActions.addChannel(payload));
-});
+  socket.on('newChannel', (payload) => {
+    store.dispatch(channelsActions.addChannel(payload));
+  });
 
-socket.on('renameChannel', ({ name, id }) => {
-  store.dispatch(channelsActions.renameChannel({ id, name }));
-});
+  socket.on('renameChannel', ({ name, id }) => {
+    store.dispatch(channelsActions.renameChannel({ id, name }));
+  });
 
-socket.on('removeChannel', ({ id }) => {
-  store.dispatch(channelsActions.removeChannel(id));
-});
+  socket.on('removeChannel', ({ id }) => {
+    store.dispatch(channelsActions.removeChannel(id));
+  });
+};
 
-document.body.classList.add('bg-light', 'h-100');
+const app = async () => {
+  await i18next
+    .use(initReactI18next)
+    .init(i18nextConfig);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+  subscribeToServerEvents();
 
-root.render(
-  <React.StrictMode>
-    <RollbarProvider config={rollbarConfig}>
-      <ErrorBoundary>
-        <Provider store={store}>
-          <LeoProfanityContext.Provider value={filter}>
-            <SocketContext.Provider value={socket}>
+  document.body.classList.add('bg-light', 'h-100');
+
+  const root = createRoot(document.getElementById('root'));
+
+  root.render(
+    <React.StrictMode>
+      <RollbarProvider config={rollbarConfig}>
+        <ErrorBoundary>
+          <Provider store={store}>
+            <LeoProfanityContext.Provider value={LeoProfanity}>
               <I18nextProvider i18n={i18next} defaultNS="translation">
                 <App />
               </I18nextProvider>
-            </SocketContext.Provider>
-          </LeoProfanityContext.Provider>
-        </Provider>
-      </ErrorBoundary>
-    </RollbarProvider>
-  </React.StrictMode>,
-);
+            </LeoProfanityContext.Provider>
+          </Provider>
+        </ErrorBoundary>
+      </RollbarProvider>
+    </React.StrictMode>,
+  );
+};
+
+app();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
